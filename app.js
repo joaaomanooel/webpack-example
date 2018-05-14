5ee1f0ff21f466c9d92a17a98c177e147387f46c
João Manoel Neto <joaaomanooel@gmail.com>
2018-05-14 18:02:23 -0300

    add production mode on webpack

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import {union, uniq} from 'ramda';
+import react from 'react';
+import reactDOM from 'react-dom';
+
 import sum, {sub, multiply, div as divide, PI } from './utils';
 
 const arr1 = [ 1, 1, 1, 2, 2, 3, 4, 5, 6, 6, ];
